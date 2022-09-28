b967a2732af8514043b463c2ef1c8605d02fd04c
Blacky <77108939+brblacky@users.noreply.github.com>
2022-09-29 00:32:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export const paginate = require('./features/paginate.js');
+export const Paginate = require('./features/paginate.js');
 export const Markdown = require('./features/markdown.js').default;
